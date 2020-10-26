b746570cf7605d7e455206f209351ff35fbcf0d1
sav4yk <mail@sav4yk.ru>
2020-10-26 21:07:13 +0300

    готовый результат

--- get-time.php
+++ get-time.php
@@ -1,83 +1,63 @@
 <?php
-
-if (isset($_POST['end']) && isset($_POST['start'])) {
+// получение и фильтрация данных (либо по умолчанию)
+if (isset($_POST['end']) && isset($_POST['start']) && $_POST['end']!='' && $_POST['start']!='') {
     $start_work = filter_var($_POST['start'], FILTER_VALIDATE_FLOAT);
     $end_work = filter_var($_POST['end'], FILTER_VALIDATE_FLOAT);
 } else {
     $start_work = 9.5;
     $end_work = 16;
 }
+// конвертируем в строку
+$start_work = toStr($start_work);
+$end_work =  toStr($end_work);
 
-date_default_timezone_set('Etc/GMT-4');
-const MSK = 7;  // если сервер настроен на UTC, то 3
-const USA = 0; // если сервер настроен на UTC, то -4
-
-$date = new DateTime();
-//echo $date->getTimestamp();
-$now_datetime = $date->getTimestamp();
-$now_datetime_msk = $date->getTimestamp() + (MSK * 3600);
-
-$get_datetime = $now_datetime;
-$get_datetime_result = '';
-$get_type = '';
+$get_datetime = new DateTime('now', new DateTimeZone('America/Guadeloupe'));
+$now_get_datetime = clone $get_datetime;    //текущая дата
+$get_datetime_result = '';  // интервал в секундах до начала(конца) работы
+$get_type = ''; // начало/конец (для светодиода и "закроются"/"откроются"
 
 while ($get_datetime_result == '') {
+    // проверка на субботу, воскресенье
     if (isWeekend($get_datetime)) {
-        $get_datetime = plusOneDay($get_datetime);
+        $get_datetime->add(new DateInterval("P1D"));
+        $get_datetime->setTime(0, 0);
         continue;
     }
+    // проверка на праздник
     if (isHoliday($get_datetime)) {
-        $get_datetime = plusOneDay($get_datetime);
+        $get_datetime->add(new DateInterval("P1D"));
+        $get_datetime->setTime(0, 0);
         continue;
     }
 
-    $curDay = getCurrentDay($get_datetime);
-    $start_day = $curDay + (3600 * ($start_work+1));
-    $end_day = $curDay + (3600 * ($end_work+1) );
+    //дата начала рабочего дня (со временем)
+    $cur_day= clone $get_datetime;
+    $start_day = $cur_day->setTime(0, 0)->modify('+' . $start_work );
+    //дата конца рабочего дня (со временем)
+    $cur_day=  clone $get_datetime;
+    $end_day = $cur_day->setTime(0, 0)->modify('+' .  $end_work);
 
     if ($get_datetime < $start_day) { // рабочий день, время до начала торгов
-        $get_datetime_result = ($start_day - $get_datetime + ($get_datetime - $now_datetime)); //до начала
+        $get_datetime_result = sumIntervals($start_day->diff($get_datetime, true),
+            $now_get_datetime->diff($get_datetime, true));
         $get_type = 'start';
     } elseif ($get_datetime > $start_day && $get_datetime < $end_day) { // рабочий день, идут торги
-        $get_datetime_result = $end_day - $get_datetime;
+        $get_datetime_result = sumIntervals($end_day->diff($get_datetime, true),
+            $now_get_datetime->diff($get_datetime, true));
         $get_type = 'end';
     } else { // рабочий день, торги закончились
-        $get_datetime = plusOneDay($get_datetime);
+        $get_datetime->add(new DateInterval("P1D"));
         continue;
     }
+    // перевод в интервал
+    $time_rest = $get_datetime_result->s + $get_datetime_result->i * 60 + $get_datetime_result->h * 3600 + $get_datetime_result->d * 24 * 3600;
+
     $json = json_encode([
-        'time_in_usa' => $now_datetime,
-        'time_in_msk' => $now_datetime_msk,
-        'time_rest' => $get_datetime_result,
+        'time_rest' => $time_rest,
         'type' => $get_type
     ]);
     echo $json;
 
-}
-
-
-/**
- * Увеличивает дату на один день
- *
- * @param $date
- * @return false|int
- */
-function plusOneDay($date)
-{
-    $new_date = strtotime('+1 day', $date);
-    $new_date = strtotime('00:00', $new_date);
-    return $new_date;
-}
-
-/**
- * Возвращает полночь текущей даты
- *
- * @param $date
- * @return false|int
- */
-function getCurrentDay($date)
-{
-    return strtotime(date('Y-m-d', $date) . ' midnight');
 }
 
 /**
@@ -88,13 +68,7 @@ function getCurrentDay($date)
  */
 function isWeekend($date)
 {
-
-
-
-    $weekday= date("l", $date );
-
-    if ($weekday =="Saturday" OR $weekday =="Sunday") { return true; }
-    else {return false; }
+    return $date->format('N')>5;
 }
 
 /**
@@ -105,5 +79,69 @@ function isWeekend($date)
  */
 function isHoliday($date)
 {
+    $currentYear = date("Y");
+    $holidays = [
+        getObservedDate(new DateTime("1 jan $currentYear")),//
+        getObservedDate(new DateTime("third monday of january $currentYear")),//
+        getObservedDate(new DateTime("third monday of feb $currentYear")),//
+        getObservedDate(new DateTime("10 apr $currentYear")),
+        getObservedDate(new DateTime("last monday of may $currentYear")),//
+        getObservedDate(new DateTime("4 july $currentYear")),
+        getObservedDate(new DateTime("first monday of september $currentYear")),
+        getObservedDate(new DateTime("last thursday of november $currentYear")),//
+        getObservedDate(new DateTime("25 dec $currentYear")),//
+    ];
+
+    foreach($holidays as $holiday) {
+        if ($holiday->diff($date)->days == 0)
+            return true;
+    }
     return false;
+}
+
+/**
+ * Смещает дату на один, если праздник попадает на субботу или воскресенье
+ *
+ * @param $holidayDate
+ * @return mixed
+ */
+function getObservedDate($holidayDate)
+{
+    $dayofweek = $holidayDate->format('N');
+
+    if ($dayofweek == 6) $holidayDate->sub(new DateInterval("P1D")); //суббота на пятницу
+    else if ($dayofweek == 0)  $holidayDate->add(new DateInterval("P1D"));  //воскресенье на понедельник
+    return $holidayDate;
+}
+
+/**
+ * Суммирует два интервала
+ *
+ * @param $first
+ * @param $second
+ * @return DateInterval|false
+ */
+function sumIntervals($first, $second)
+{
+    $dt = new DateTime();
+    $dt_diff = clone $dt;
+    $dt->add($first);
+    $dt->add($second);
+    return $dt->diff($dt_diff);
+}
+
+/**
+ * Конвертирует число в строку
+ * 9 => 9 hours, 9.5 => 9 hours 30 minutes
+ *
+ * @param $num
+ * @return string
+ */
+function toStr($num) {
+    $c = floor($num);
+    $d = ($num - $c)*60;
+    if ($d>0)
+        return $c . ' hours ' . $d . ' minutes';
+    else
+        return $c . ' hours ';
 }
--- index.php
+++ index.php
@@ -13,37 +13,39 @@
 <div>Начало торгов: <input type="text" id="start" name="start" value="9.5"></div>
 <div>Конец торгов: <input type="text" id="end" name="end" value="16"></div>
 <button id="button">Получить</button>
-<div>Время в MSK: <input type="text" id="time_msk" name="time_msk" value="" readonly></div>
-<div>Время на биржах: <input type="text" id="time_usa" name="time_usa" value="" readonly></div>
-<div class="message"><div class="red" id="indicator"></div>
+
+<div class="message">
+    <div class="red" id="indicator"></div>
     <div id="text"></div>
 </div>
 
 <script>
     function formatTime(seconds) {
-        const d = Math.floor(seconds / 3600 / 24);
-        console.log(d);
-        const h = Math.floor(seconds / 3600);
+        d = Math.floor(seconds / 86400);
+        if (d == 2) d = d.toString() + ' дня ';
+        if (d == 1) d = d.toString() + ' день ';
+        if (d == 0) d = '';
+        const h = Math.floor(seconds / 3600) % 24;
         const m = Math.floor((seconds % 3600) / 60);
         const s = Math.round(seconds % 60);
-        return [
-            d,
+
+        return d + ' ' + [
             h,
             m > 9 ? m : (h ? '0' + m : m || '0'),
             s > 9 ? s : '0' + s
         ].filter(Boolean).join(':');
     }
 
-    function convert(unixtime){
-        var months_arr = ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'];
-        var date = new Date(unixtime*1000);
+    function convert(unixtime) {
+        var months_arr = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
+        var date = new Date(unixtime * 1000);
         var year = date.getFullYear();
         var month = months_arr[date.getMonth()];
         var day = date.getDate();
         var hours = date.getHours();
         var minutes = "0" + date.getMinutes();
         var seconds = "0" + date.getSeconds();
-        return  day + ' ' + month + ' ' + year + 'г. ' + hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+        return day + ' ' + month + ' ' + year + 'г. ' + hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
 
     }
 
@@ -56,9 +58,10 @@
             data: 'start=' + $('#start').val() + '&end=' + $('#end').val(),
             success: function (result) {
                 var data = JSON.parse(result);
-                i=0;
-                interval = setInterval(function() {
+                i = 0;
+                interval = setInterval(function () {
                     if (data.time_rest < 100) {
+                        clearInterval(interval);
                         updateData(); //если время вот-вот кончится, отправляем еще один запрос на сервер
                     }
                     if (data.type == 'end') {
@@ -73,9 +76,6 @@
                         $('#indicator').addClass('red');
                         $('#indicator').removeClass('green');
                     }
-                    $('#time_msk').val(convert(data.time_in_msk - i));
-                    $('#time_usa').val(convert(data.time_in_usa - i));
-
                     i++;
                 }, 1000);
             }
@@ -83,12 +83,8 @@
     }
 
     var interval = null;
-
-
-
-    $('#button').click(function() {
+    $('#button').click(function () {
         updateData();
-
     });
 </script>
 </body>
